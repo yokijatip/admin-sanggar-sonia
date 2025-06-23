5d73593b82b5bb13f140aedfa3d35de94c435e9f
Yoki Jati Perkasa <yokijati@gmail.com>
2025-06-23 17:36:55 +0700

    lesgow

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false, // Full SPA mode
+  // atau bisa hybrid dengan nitro presets
+  nitro: {
+    preset: "static", // jika mau deploy sebagai static files
+  },
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/icon", "@nuxt/fonts", "@nuxt/image"],
+  // Add Tailwind
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/css/main.css"],
+});
