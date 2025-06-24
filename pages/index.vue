94e00c1b087c994624d503a270ddb0f3b52176d4
Yoki Jati Perkasa <yokijati@gmail.com>
2025-06-25 04:35:42 +0700

    Success add date range picker

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import DateRangePicker from "@/components/DateRangePicker.vue";
+</script>
 
 <template>
   <div>
-    <h1>Dashboard Index.vue</h1>
+    <!-- Top Bar (Tabs Menu & Date Range Picker & Download Button) -->
+    <div>
+      <DateRangePicker />
+    </div>
+
+    <!-- Content Dashboard -->
+    <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+      <div class="grid auto-rows-min gap-4 md:grid-cols-4">
+        <div class="aspect-video rounded-xl bg-muted/50"></div>
+        <div class="aspect-video rounded-xl bg-muted/50" />
+        <div class="aspect-video rounded-xl bg-muted/50" />
+        <div class="aspect-video rounded-xl bg-muted/50" />
+      </div>
+      <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+    </div>
   </div>
 </template>
