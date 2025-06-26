17cbd2cc0c30cd41d38fb9bef87c7b1a83ac6c1c
Yoki Jati Perkasa <yokijati@gmail.com>
2025-06-27 00:46:10 +0700

    Add Orders Finish

---
+++ components/ui/HeadersContent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="mb-8">
+      <h1 class="text-3xl font-bold tracking-tight">{{ title }}</h1>
+      <p class="text-muted-foreground">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// Props untuk menerima data dari komponen induk
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style></style>
